3686372c8077bf8f2010cdb9b3e540e3c599e210
Vive1113 <120009949+Vive1113@users.noreply.github.com>
2023-03-11 12:06:47 +0530

    3rd commit

---
+++ Console_Project/src/MainClass.java
@@ -0,0 +1,11 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		//			Scanner scan = new Scanner(System.in);
+		//Object is created for Student class
+		Student student = new Student();
+	}
+}
+
+
